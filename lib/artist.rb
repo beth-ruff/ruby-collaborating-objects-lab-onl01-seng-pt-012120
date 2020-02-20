68a71a19ad68b8a16c583b7af056136602747c33
Beth Ruff <beth.ruff2@gmail.com>
2020-02-20 02:23:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,7 +21,7 @@ class Artist
     Song.all.select {|song| song.artist == self}
   end 
   
-  def find_or_create_by_name(artist_name)
+  def self.find_or_create_by_name(artist_name)
     if self.find(artist_name)
       self.find(artist_name)
     else 
