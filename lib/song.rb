e5daca630aaf69b6d84d32002c9d187e73e43f6c
Beth Ruff <beth.ruff2@gmail.com>
2020-02-20 03:05:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,14 @@ class Song
     @@all 
   end 
   
+  def artist_name=(name)
+    Artist.find_or_create_by(name)
+  end 
+  
   def self.new_by_filename(name)
     split_name = name.split(" - ")
     song = self.new(split_name[1]) 
+    song.artist_name = split_name[0]
   end 
   
 end 
